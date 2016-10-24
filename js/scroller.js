0e82ab807d437e5e3a07dce47adcacaa04ea83ae
Casey Cabrales <caseycabrales@gmail.com>
2016-10-23 19:33:05 -0700

    Scrolling through page highlights nav bar item that user is closest to

--- js/scroller.js
+++ js/scroller.js
@@ -2,6 +2,10 @@
 
 var navBar = document.getElementById('top-nav'),
     navItems = navBar.getElementsByTagName('a'),
+    navBarHeight = navBar.offsetHeight,
+    prevSelected = navBar.getElementsByClassName('active')[0],
+    scrollItems = [],
+    lastId,
     scrollOptions = {
         // duration of the scroll per 1000px, default 500
         speed: 600,
@@ -18,16 +22,60 @@ var navBar = document.getElementById('top-nav'),
     };
 
 for (var i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener('click', function(e) {
+    var item = navItems[i];
+
+    var anchor = item.getAttribute('href');
+    scrollItems.push(getElementFromAnchor(anchor));
+
+    item.addEventListener('click', function(e) {
         e.preventDefault();
         scrollClick(this);
     });
 }
 
+function getElementFromAnchor(anchor) {
+    return document.getElementById(anchor.slice(1));
+}
+
+//Get the div and offset of that div to scroll to
+//Also unset and set the active class on current nav item
 function scrollClick(item) {
-    var anchor = item.getAttribute('href').slice(1),
-        elem = document.getElementById(anchor),
-        offset = elem.offsetTop - navBar.offsetHeight;
+    var anchor = item.getAttribute('href'),
+        elem = getElementFromAnchor(anchor),
+        offset = elem.offsetTop - navBarHeight;
+
+    prevSelected.classList.remove('active');
+    prevSelected = item.parentNode;
+    prevSelected.classList.add('active');
+    lastId = anchor;
 
     animateScrollTo(offset, scrollOptions);
 }
+
+//Bind to scroll to set navbar highlighted item
+window.onscroll = function() {
+    // Get container scroll position
+    var fromTop = document.documentElement.scrollTop + navBarHeight;// + navBarHeight;// + 300; //Edit this + number for smaller/larger windows to change it
+    if ((window.innerHeight + window.scrollY) > document.body.offsetHeight) {
+        fromTop = document.body.offsetHeight;
+    }
+
+    // Get id of current scroll item
+    var cur = scrollItems.filter(function(val){
+        // if the div offset top is less than the current scroll plus 1/3 the window height
+        if ((val.offsetTop - navBarHeight) < (fromTop + (window.innerHeight/3)))
+            return val;
+    });
+
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur ? cur.id : "about";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        prevSelected.classList.remove('active');
+        prevSelected = document.getElementById(id + '-nav').parentNode;
+        prevSelected.classList.add('active');
+    }
+};
